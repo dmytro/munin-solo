fb3c9bee4c490508cc23b05e8fd1ac97b9e9a94d
Tiago Amaro <tiagocis@gmail.com>
2013-12-22 19:44:16 -0200

    fixing typo on metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -19,5 +19,5 @@ supports 'scientific'
 supports 'ubuntu'
 
 recipe 'munin', 'Empty, use one of the other recipes'
-recipe 'munin::client', 'Instlls munin and configures a client by searching for the server, which should have a role named monitoring'
+recipe 'munin::client', 'Installs munin and configures a client by searching for the server, which should have a role named monitoring'
 recipe 'munin::server', 'Installs munin and configures a server, node should have the role "monitoring" so clients can find it'
